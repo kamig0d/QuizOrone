c2539825d1cbb6fd2d2dc16e7a964fa65c53798a
kamig0d <juninhobarbosa123@hotmail.com>
2019-06-04 01:04:35 -0300

    mudando nome do projeto na rota

--- node.js
+++ node.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static(__dirname + '/dist/quiz'));
+app.use(express.static(__dirname + '/dist/quizorone'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/quiz/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/quizquizorone/index.html'));
 });
