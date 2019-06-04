018c2e08bf593986b3e49e184c259dd69cdef7d8
kamig0d <juninhobarbosa123@hotmail.com>
2019-06-04 01:13:58 -0300

    alterando nome do projeto

--- node.js
+++ node.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static(__dirname + '/dist/quizorone'));
+app.use(express.static(__dirname + '/dist/quiz'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/quizorone/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/quiz/index.html'));
 });
