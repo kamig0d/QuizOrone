5694327c454d1a7581434372d72de61efa6a031b
kamig0d <juninhobarbosa123@hotmail.com>
2019-06-04 01:04:53 -0300

    ajustando nome

--- node.js
+++ node.js
@@ -7,5 +7,5 @@ app.use(express.static(__dirname + '/dist/quizorone'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/quizquizorone/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/quizorone/index.html'));
 });
